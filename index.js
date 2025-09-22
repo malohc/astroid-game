8e6a7171db0ac03fb484c2272146629c0705e538
Malik-hc <malikhcunningham6@gmail.com>
2025-09-22 17:05:11 -0400

    Create player

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('hellow orld')
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+context.fillRect(0, 0, canvas.width, canvas.height)
+context.fillStyle = 'black'
+
+class Player {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity 
+    }
+
+    draw() {
+        context.moveTo(this.position.x + 30, this.position.y)
+        context.lineTo(this.position.x - 10, this.position.y - 10)
+        context.lineTo(this.position.x - 10, this.position.y + 10)
+        context.closePath()
+
+        context.strokeStyle = 'white'
+        context.stroke()
+    }
+}
+
+const player = new Player({
+    position: { x: canvas.width / 2 , y: canvas.height / 2},
+    velocity: { x: 0, y: 0}
+})
+
+player.draw()
