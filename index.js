b56a12b2b7c2e305ff17f6a35daf9ad233f5da36
Malik-hc <malikhcunningham6@gmail.com>
2025-09-23 16:14:08 -0400

    Added Shooting function and Astroid spawning

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
 
 const SPEED = 2
+const PROJECTILE_SPEED = 3
 const ROTATION = 0.05
 const DECCEL = 0.99
+const RADIUS = 5
+const SPAWN_TIME = 3000
+const MAX_SIZE = 50
+const MIN_SIZE = 10
+
+const keys = {
+    up: {
+        pressed: false
+    },
+
+    right: {
+        pressed: false
+    },
+
+    left: {
+        pressed: false
+    }
+}
+
+const projectiles = []
+const astroids = []
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
@@ -17,12 +39,16 @@ class Player {
 
     draw() {
         context.save()
+
         context.translate(this.position.x, this.position.y)
         context.rotate(this.rotation)
-         context.translate(-this.position.x, -this.position.y)
+        context.translate(-this.position.x, -this.position.y)
+
+        context.beginPath()
         context.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false)
         context.fillStyle = 'red'
         context.fill()
+        context.closePath
 
         context.beginPath()
         context.moveTo(this.position.x + 30, this.position.y)
@@ -47,19 +73,102 @@ const player = new Player({
     velocity: { x: 0, y: 0}
 })
 
-const keys = {
-    up: {
-        pressed: false
-    },
+class Projectile {
+    constructor ({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.radius = RADIUS
+    }
 
-    right: {
-        pressed: false
-    },
+    draw() {
+        context.beginPath()
+        context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false)
+        context.closePath()
+        context.fillStyle = 'white'
+        context.fill()
+    }
 
-    left: {
-        pressed: false
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
     }
 }
+
+class Astroid {
+    constructor ({position, velocity, radius}) {
+        this.position = position
+        this.velocity = velocity
+        this.radius = radius
+    }
+
+    draw() {
+        context.beginPath()
+        context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false)
+        context.closePath()
+        context.strokeStyle = 'white'
+        context.stroke()
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
+window.setInterval(() => {
+    const index = Math.floor(Math.random() * 4)
+    let radius = MAX_SIZE * Math.random() + MIN_SIZE
+    let x, y
+    let vx, vy
+
+
+    switch (index) { 
+        case 0: //Left side of screen
+            x = 0 - radius
+            y = Math.random() * canvas.height
+            vx = 1
+            vy = 0
+            break
+
+        case 1: //Bottom side of screen
+            x = Math.random() * canvas.width
+            y = canvas.height + radius
+            vx = 0
+            vy = 1
+            break
+
+        case 2: //Right side of screen
+            x = canvas.width + radius
+            y = Math.random() * canvas.height
+            vx = -1
+            vy = 0
+            break
+
+        case 3: //Top side of screen
+            x = Math.random() * canvas.width
+            y = 0 - radius
+            vx = 0
+            vy = 1
+            break
+    }
+
+    astroids.push(
+        new Astroid ({
+            position: {
+                x: x,
+                y: y,
+            },
+
+            velocity: {
+                x: vx,
+                y: vy,
+            },
+
+            radius
+        }))
+}, SPAWN_TIME)
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -68,6 +177,35 @@ function animate() {
     context.fillRect(0, 0, canvas.width, canvas.height)
 
     player.update()
+
+    //Projectile Management
+    for (let i = projectiles.length - 1; i >= 0; i --) {
+        const projectile = projectiles[i]
+        projectile.update()
+
+        if (projectile.position.x + projectile.radius < 0 || 
+            projectile.position.x - projectile.radius > canvas.width ||
+            projectile.position.y - projectile. radius > canvas.height ||
+            projectile.position.y + projectile.radius < 0
+        ) {
+            projectiles.splice(i, 1)
+        }
+    }
+
+    //Astroid Management
+    for (let i = astroids.length - 1; i >= 0; i --) {
+        const astroid = astroids[i]
+        astroid.update()
+
+
+        if (astroid.position.x + astroid.radius < 0 || 
+            astroid.position.x - astroid.radius > canvas.width ||
+            astroid.position.y - astroid. radius > canvas.height ||
+            astroid.position.y + astroid.radius < 0
+        ) {
+            projectiles.splice(i, 1)
+        }
+    }
     
     if (keys.up.pressed) {
         player.velocity.x = Math.cos(player.rotation) * SPEED
@@ -97,6 +235,21 @@ window.addEventListener('keydown', (event) => {
             break
         case 'ArrowRight':
             keys.right.pressed = true
+            break
+        case 'Space':
+            projectiles.push(
+                new Projectile ({
+                    position: {
+                        x: player.position.x + Math.cos(player.rotation) * 30,
+                        y: player.position.y + Math.sin(player.rotation) * 30,
+                    },
+
+                    velocity: {
+                        x: Math.cos(player.rotation) * PROJECTILE_SPEED,
+                        y: Math.sin(player.rotation) * PROJECTILE_SPEED,
+                    },
+                })
+            )
             break
     }
 })
